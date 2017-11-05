a432d3b48e9ceac8e2127fad77ff4e77f7b2b112
Erika Solomon <mrserikasolomon@gmail.com>
2017-11-05 23:19:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code in this file!
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+'Edsger Dijkstra'.length;
+//=> 15
+
+'Edsger Dijkstra'[15];
+//=> undefined
+
+'Edsger Dijkstra'[14];
+//=> "a"
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
