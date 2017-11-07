33506628d128332d465514e71d42ad753ebc28fa
mary huynh <mhuynhh@gmail.com>
2017-11-07 22:23:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// Write your code in this file!
 const currentUser = 'Grace Hopper';
-let welcomeMessage = ('Welcome to Flatbook, ' + currentUser + '!')
-let excitedWelcomeMessage = welcomeMessage.toUpperCase()
-const shortGreeting = 'Welcome, ' + currentUser.slice(0,1) + '!'
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
