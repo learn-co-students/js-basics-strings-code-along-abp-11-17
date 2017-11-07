8f7f5ef749b58d45e9f5520f844807a8585bdbb6
Demetrios Kalcassinas <disinas@icloud.com>
2017-11-07 12:54:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+
+let currentUser = 'Demetrios';
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + `!`;
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, ";
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+excitedWelcomeMessage += upperCaseCurrentUser;
+excitedWelcomeMessage += '!';
+
+shortGreeting = "Welcome, ";
+const firstInitial = currentUser[0];
+shortGreeting = shortGreeting + firstInitial + '!';
