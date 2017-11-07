15e27d7cfb8de7c049f6e5459d3de8f765dad346
Osbaldo Martinez Jr <martinez.osbaldo@gmail.com>
2017-11-07 18:03:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser="Whatever Name";
+const welcomeMessage="Welcome to Flatbook, "+ currentUser + "!";
+const excitedWelcomeMessage=welcomeMessage.toUpperCase();
+const shortGreeting="Welcome, " +"!"
