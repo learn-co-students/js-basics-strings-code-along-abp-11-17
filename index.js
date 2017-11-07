628a15de51855a0eedb306fea2e16d91b5348b53
Meenakshi Radhakrishnan <junnamma@yahoo.com>
2017-11-07 20:46:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Write your code in this file!
-const currentUser = " ";
+const currentUser = "Marie Curie";
 const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
 const excitedWelcomeMessage = `welcome to flatbook, " + ${currentUser}!`.toUpperCase();
-const firstInitial = currentUser[0];
+//const firstInitial = currentUser[0];
+//const firstInitial = currentUser.slice(0,1);
+//const firstInitial = currentUser.charAt(0);
+//const firstInitial = currentUser.substr(0,1);
+const firstInitial = currentUser.substring(0,1);
 const shortGreeting = `Welcome, ${firstInitial}!`;
