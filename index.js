05beb201b7e686c0fc90e05d05ace77b092a7e84
Matthew Garrett <mdgarrett1984@gmail.com>
2017-11-07 04:12:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = 'Matt';
+let welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = 'Welcome, M!';
