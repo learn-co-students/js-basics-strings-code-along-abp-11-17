e40dee6071bc387a609e7edd7502a36dd17d7604
Maverick Tesini <mavericktesini@gmail.com>
2017-11-08 19:07:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = 'Mav';
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = 'Welcome, ' + currentUser.charAt(0) + "!";
