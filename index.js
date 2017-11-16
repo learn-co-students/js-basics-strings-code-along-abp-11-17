b60db9a7e6c02371d0f1e4ca420b58c65e756e8c
Chris Terry <terry.christopher@gmail.com>
2017-11-16 21:34:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser= 'Chris Terry'
+const welcomeMessage = "Welcome to Flatbook, Chris Terry!";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, CHRIS TERRY!"
+const shortGreeting = "Welcome, C!"
